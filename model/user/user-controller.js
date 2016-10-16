1fb50dd28cb647da9a391509d06ca526d741931d
Thomas Strauss <thomas@bytesource.net>
2016-10-16 17:28:57 +0200

    tested the project with docker and postman

--- model/user/user-controller.js
+++ model/user/user-controller.js
@@ -2,6 +2,7 @@ const Controller = require('../../lib/controller');
 const userModel  = require('./user-facade');
 
 
-class UserController extends Controller {}
+class UserController extends Controller {
+}
 
 module.exports = new UserController(userModel);
